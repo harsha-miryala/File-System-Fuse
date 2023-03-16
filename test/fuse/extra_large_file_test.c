17df76582e73c13a0375ea6a54de9a8232799b0e
root <root@ip-172-31-27-155.compute.internal>
2023-03-16 02:26:47 +0000

    Misc

--- test/fuse/extra_large_file_test.c
+++ test/fuse/extra_large_file_test.c
@@ -1,14 +1,42 @@
+#include <fcntl.h>
+#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
 
-int main() {
-    long long int size = 6LL * 1024 * 1024 * 1024;  // 6 GB in bytes
-    FILE *fp = fopen("file.bin", "wb");
-    if (fp == NULL) {
-        printf("Error: unable to create file.\n");
-        return 1;
+#define ITER_COUNT 250000
+
+int main(int agrc, char **argv){
+    //opening the log file
+    int log_file = open("ExtraLargeFileLog.txt", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+    int fd, res=-1;
+    char msg[4096];
+    memset(msg, 'h', 4096);
+
+    //open the large file
+    fd = open("extra_large_file.txt", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+    if(fd==-1){
+        sprintf(msg, "Failure to open/create large file \'large_file.txt\'");
+        res=write(log_file, msg, strlen(msg));
+        return 0;
     }
-    fseek(fp, size - 1, SEEK_SET);
-    fputc(0, fp);
-    fclose(fp);
+
+    // Large file is successfully opened here. Start writing to it.
+    for(int i=0;i< ITER_COUNT;i++){
+        res = write(fd,msg,strlen(msg));
+        if(res==-1){
+            memset(msg,0,4096);
+            sprintf(msg, "Error writing on large File in iter %d\n",i);
+            res=write(log_file, msg, strlen(msg));
+            return 0;
+        }
+    }
+    // writing to log file is finished. Update the log file.
+    memset(msg, 0, 4096);
+    sprintf(msg,"SUCCESS! FILE SIZE: %d KB\n", ITER_COUNT);
+    res=write(log_file, msg, strlen(msg));
+
+    close(fd);
+    close(log_file);
     return 0;
 }
